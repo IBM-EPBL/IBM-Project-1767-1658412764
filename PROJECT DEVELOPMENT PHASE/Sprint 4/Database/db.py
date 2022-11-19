8e5a5119d3062cb1d6b1467b2320d7c439114af7
Jayanthika-8 <100111241+Jayanthika-8@users.noreply.github.com>
2022-11-19 13:37:30 +0530

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/Sprint 4/Database/db.py
@@ -0,0 +1,3 @@
+import ibm_db
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=815fa4db-dc03-4c70-869a-a9cc13f33084.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30367;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=hvv22243;PWD=iXmuhK8KgBWxkJWb;",'','')
+print('connection succesful')
